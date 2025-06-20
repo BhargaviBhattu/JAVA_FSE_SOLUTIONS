59bdeadbe6779590c9b28d011683e27ae72f873e
BhargaviBhattu <bhargavibhattu18@gmail.com>
2025-06-20 17:52:04 +0530

    Create YearlyData.java

---
+++ WEEK1/Week_1_DataStructuresAndAlgorithms/02_FinancialForecasting/YearlyData.java
@@ -0,0 +1,17 @@
+public class YearlyData {
+    private int year;
+    private double value;
+
+    public YearlyData(int year, double value) {
+        this.year = year;
+        this.value = value;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public double getValue() {
+        return value;
+    }
+}
