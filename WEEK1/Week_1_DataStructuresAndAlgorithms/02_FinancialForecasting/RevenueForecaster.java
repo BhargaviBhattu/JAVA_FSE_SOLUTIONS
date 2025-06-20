484f611c44e029d3780d1f4b34ae7c54adc584a7
BhargaviBhattu <bhargavibhattu18@gmail.com>
2025-06-20 17:58:35 +0530

    Update RevenueForecaster.java

--- WEEK1/Week_1_DataStructuresAndAlgorithms/02_FinancialForecasting/RevenueForecaster.java
+++ WEEK1/Week_1_DataStructuresAndAlgorithms/02_FinancialForecasting/RevenueForecaster.java
@@ -3,7 +3,7 @@ import java.util.Map;
 
 public class RevenueForecaster {
     private double initialValue;
-    private double annualGrowthRate; // in decimal form (e.g. 0.10 for 10%)
+    private double annualGrowthRate; 
     private Map<Integer, Double> memo = new HashMap<>();
 
     public RevenueForecaster(double initialValue, double annualGrowthRate) {
