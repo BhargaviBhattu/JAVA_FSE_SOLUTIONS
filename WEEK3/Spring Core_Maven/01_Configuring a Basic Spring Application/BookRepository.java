6e54d3325d9e36a0a27dd8e15dae6ca2df95f7c9
BhargaviBhattu <bhargavibhattu18@gmail.com>
2025-07-01 19:06:24 +0530

    Create BookRepository.java

---
+++ WEEK3/Spring Core_Maven/01_Configuring a Basic Spring Application/BookRepository.java
@@ -0,0 +1,11 @@
+package com.library.repository;
+
+public class BookRepository {
+    public BookRepository() {
+        System.out.println("BookRepository: Created a new Repository");
+    }
+
+    public void saveBook() {
+        System.out.println("BookRepository: Saving book...");
+    }
+}
