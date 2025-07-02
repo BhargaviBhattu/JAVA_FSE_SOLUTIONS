261c818a0d0c6680eda0c525335e4443e0745380
BhargaviBhattu <bhargavibhattu18@gmail.com>
2025-07-02 16:48:03 +0530

    Create BookRepository.java

---
+++ WEEK3/Spring Core_Maven/05_Configuring the Spring IoC Container/BookRepository.java
@@ -0,0 +1,11 @@
+package com.library.repository;
+
+public class BookRepository {
+    public BookRepository() {
+        System.out.println("[XML] BookRepository: initialized");
+    }
+
+    public void saveBook() {
+        System.out.println("[XML] BookRepository: saving book...");
+    }
+}
