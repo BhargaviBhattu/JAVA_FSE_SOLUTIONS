257f627bd1a969594b44449e17c6767a203cf884
BhargaviBhattu <bhargavibhattu18@gmail.com>
2025-07-04 20:55:15 +0530

    Delete WEEK3/Spring Data JPA with Spring Boot, Hibernate/01_Spring Data JPA - Quick Example directory

--- WEEK3/Spring Data JPA with Spring Boot, Hibernate/01_Spring Data JPA - Quick Example/DepartmentRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.ems.repository;
-
-import com.example.ems.model.Department;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface DepartmentRepository extends JpaRepository<Department, Long> {
-}
-
