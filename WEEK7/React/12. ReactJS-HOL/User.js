13ac0bf029094f31dcc5cc23de915f7401789638
BhargaviBhattu <bhargavibhattu18@gmail.com>
2025-07-28 15:47:03 +0530

    Add files via upload

---
+++ WEEK7/React/12. ReactJS-HOL/User.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function User() {
+  return (
+    <div>
+      <h2>Welcome Back, User! 🎟️</h2>
+      <p>You are now logged in. You can proceed to book your flight tickets.</p>
+    </div>
+  );
+}
+
+export default User;
